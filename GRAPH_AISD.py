368b22033972d8dfdcfee9f9e89c20ca1f3b9834
ColdBacon <bogumila.walkowiak@op.pl>
2019-05-21 19:11:51 +0200

    something went wrong with hamiltonian graph

--- GRAPH_AISD.py
+++ GRAPH_AISD.py
@@ -35,7 +35,6 @@ while edge < MAXedge:
 print ("PRAWIE DONE: ")
 for i in range(n):
     print (matrix[i])
-
 for i in range(n):
     x = random.randint(0, n - 1)
     y = random.randint(0, n - 1)
@@ -91,51 +90,6 @@ def del_connection(matrix, x, y):
     matrix[x][y] = 0
     matrix[y][x] = 0
 
-#*******************************************
-#NIEFAJNE HAMILTONY
-def Hamilton1(matrix,v,V,result):
-    V.append(v)
-    for w in range(len(matrix[v])):
-        if matrix[v][w] == 1 and w not in V:
-            del_connection(matrix, v, w)
-            result = Hamilton1(matrix, w, V, result)
-            if result:
-                return result
-    if len(V) == len(matrix) and matrix[v][0] == 1:
-        return V+[0]
-    else:
-        V.pop()
-        if len(V) > 1:
-            last = V[-1]
-            make_connection(matrix, v, last)
-#print ("JEDEN CYKL HAMILTONA: ")
-#print (Hamilton1(matrix[:],0,[],[]))
-
-def Hamilton2(matrix, v, V, result, RESULT):
-    V.append(v)
-    for w in range(len(matrix[v])):
-        if matrix[v][w] == 1 and w not in V:
-            del_connection(matrix, v, w)
-            result = Hamilton2(matrix[:], w, V, result, RESULT)
-            if result:
-                RESULT.append(result)
-                V.pop()
-                if len(V) > 0:
-                    last = V[-1]
-                    make_connection(matrix, v, last)
-    if len(V) == len(matrix) and matrix[v][0] == 1:
-        return V+[0]
-    else:
-        V.pop()
-        if len(V) > 0:
-            last = V[-1]
-            make_connection(matrix, v, last)
-
-print ("PROSZĘ WIĘCEJ HAMILTONÓW: ")
-#RESULT = []
-#Hamilton2(matrix[:],0,[],[],RESULT)
-#print(RESULT)
-
 MATRIX = [
 [0,1,1,0,0,0],
 [1,0,1,1,1,0],
@@ -145,21 +99,41 @@ MATRIX = [
 [0,0,0,1,1,0],
 ]
 
-def cykl_hamiltona(matrix,v,V,result):
+#*******************************************
+#ROBI OK, ALE USUWA MACIERZ
+def Hamilton(matrix,v,V,result):
+    V.append(v)
+    for w in range(len(matrix[v])):
+        if matrix[v][w] == 1 and w not in V:
+            del_connection(matrix, v, w)
+            result = Hamilton(matrix, w, V, result)
+            if result:
+                return result
+    if len(V) == len(matrix) and matrix[v][0] == 1:
+        return V+[0]
+    else:
+        V.pop()
+        if len(V) > 1:
+            last = V[-1]
+            make_connection(matrix, v, last)
+#print (Hamilton(matrix,0,[],[]))
+
+RESULT=[]
+def multiHamilton(matrix,v,V,RESULT):
     if len(V)==len(matrix) and matrix[v][0]:
-        result.append(V)
-        print (result)
+        RESULT.append(V+[0])
     if not len(V):
         V.append(0)
     for w in range(len(matrix[v])):
         if matrix[v][w] and w not in V:
             V.append(w)
-            cykl_hamiltona(matrix,w,V,result)
+            multiHamilton(matrix,w,V,RESULT)
             V.pop()
 
-cykl_hamiltona(MATRIX,0,[],[])
+print ("WIELE HAMILTONÓW")
+multiHamilton(matrix,0,[],RESULT)
+print(RESULT)
 
-#jakis Euler, moze sie przydac xD
 def Euler(matrix, sv):
         out = []
         stack = [sv]
@@ -181,6 +155,18 @@ def Euler(matrix, sv):
             for j in range(len(matrix)):
                 matrix[i][j]*=-1
         return out
-
 print ("EULER")
 print(Euler(MATRIX,0))
+
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+result=[]
+def Hamilton(matrix,v,V,result):
+    V.append(v)
+    for i in range(len(matrix)):
+        if i not in V and matrix[v][i]:
+            Hamilton(matrix,i,V,result)
+    if(len(V)==len(matrix)) and (matrix[v][V[0]]):
+        result.append(V+[0])
+    else:
+        V.remove(v)
+#Hamilton(matrix,0,[],result)
